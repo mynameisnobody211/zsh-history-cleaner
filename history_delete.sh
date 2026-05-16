d6356bf03f32a122cef34ecb04ccdf01d08db732
mynameisnobody211 <mynameisnobody211@proton.me>
2026-05-16 15:17:31 +0200

    Add files via upload

---
+++ history_delete.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+print_and_clear() {
+    local text="$1"
+    local filter="$2"
+    local delay="${3:-1}"
+    local lines=$(toilet -f small "$text" | wc -l)
+    toilet -f small $filter "$text"
+    sleep $delay
+    for i in $(seq 1 $lines); do
+        tput cuu1
+        tput el
+    done
+}
+
+
+cat << 'EOF'
+
+⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⡤⢤⣤⣤⣤⣤⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣿⡿⣟⠯⡒⢯⣽⣓⣒⢾⣯⣭⣿⣿⠿⠭⠭⣯⣷⣦⡀⠀⠀⠀
+⠀⠀⠀⠀⣰⣿⣯⣞⣕⣽⠾⠿⠿⠿⢿⣏⣿⣿⣿⡗⣽⣿⣿⣷⡝⣿⣿⡆⠀⠀
+⠀⠀⠀⣀⣛⠛⢿⣛⢝⢁⣀⣀⣀⠓⠶⠈⣿⣿⡿⠗⠉⠁⢀⣀⣹⣛⣛⣳⢄⠀
+⠀⡔⡾⢁⣴⡆⢦⣬⣙⣛⣋⣤⣿⣿⣷⣾⣿⣿⣿⡆⢿⣿⡟⠻⠛⡉⣍⣲⢱⠁
+⠀⣇⣇⢸⣉⡀⢦⣌⡙⠻⠿⣯⣭⣥⠡⡤⠿⢿⣿⣿⡆⠉⡻⢿⣿⠇⢻⣟⠼⠀
+⠀⠈⠪⣴⣿⣧⡀⢉⠛⠘⢶⣦⣬⠉⣀⠓⠿⠿⠯⢉⣴⠿⠿⠓⡁⡄⠀⣿⠃⠀
+⠀⠀⠀⠙⣿⣿⣷⣌⠻⢠⣤⣀⠉⠐⠛⠿⠿⠰⠶⠦⠰⠶⠇⠘⠃⠁⠀⣿⠀⠀
+⠀⠀⠀⠀⠘⢿⣿⣿⣷⣌⠻⢿⠇⣼⣶⣦⡄⣄⣀⡀⢀⡀⢀⡀⡀⠀⢠⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠙⠯⣛⠭⣻⠶⣬⣉⣛⠛⠃⠿⠿⠃⠿⠃⠚⣀⣁⣤⣾⣿⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠯⣶⣋⡽⢛⣿⣯⣿⣭⣭⡿⢿⣿⣻⣾⢟⣿⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⠿⣶⣾⣿⣿⣿⣭⣭⣭⣶⣿⡿⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠋⠁⠀⠀
+EOF
+
+print_and_clear "YOUR HISTORY" "--metal" 1
+print_and_clear "WILL BE ERASED" "--metal" 1
+print_and_clear "STAY SAFE" "--metal" 1
+print_and_clear "HACKERS-ARISE" "--metal" 4
+
+# clear history
+export HISTFILE=/dev/null
+HISTSIZE=0
+SAVEHIST=0
+history -p 2>/dev/null
+[ -f ~/.zsh_history ] && sh -c 'shred -fzun 100 ~/.zsh_history'
+[ -f ~/.bash_history ] && sh -c 'shred -fzun 100 ~/.bash_history'
+exit 0
